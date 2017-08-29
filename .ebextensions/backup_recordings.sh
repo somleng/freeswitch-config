0bc1aed0005bec8262390779686a9f7f927a5e60
David Wilkie <dwilkie@gmail.com>
2017-08-29 13:14:32 +0700

    Only backup-recordings if there's a running freeswitch instance. #12

--- .ebextensions/backup_recordings.sh
+++ .ebextensions/backup_recordings.sh
@@ -11,10 +11,12 @@ FREESWITCH_CONTAINER_NAME="freeswitch"
 CONTAINER_PATH_KEY="org.somleng.freeswitch.recordings.container-path"
 S3_PATH_KEY="org.somleng.freeswitch.recordings.s3-path"
 
-FREESWITCH_CONTAINER_ID=$(docker ps -aqf "label=${AWS_ECS_CONTAINER_NAME_KEY}=${FREESWITCH_CONTAINER_NAME}")
+freeswitch_container_id=$(docker ps -aqf "label=${AWS_ECS_CONTAINER_NAME_KEY}=${FREESWITCH_CONTAINER_NAME}")
 
-LOCAL_PATH=$(docker inspect --format "{{ index .Config.Labels \"${CONTAINER_PATH_KEY}\"}}" $FREESWITCH_CONTAINER_ID)
+if [[ -z "$freeswitch_container_id" ]]
+  local_path=$(docker inspect --format "{{ index .Config.Labels \"${CONTAINER_PATH_KEY}\"}}" $freeswitch_container_id)
 
-S3_PATH=$(docker inspect --format "{{ index .Config.Labels \"${S3_PATH_KEY}\"}}" $FREESWITCH_CONTAINER_ID)
+  s3_path=$(docker inspect --format "{{ index .Config.Labels \"${S3_PATH_KEY}\"}}" $freeswitch_container_id)
 
-docker run --volumes-from $FREESWITCH_CONTAINER_ID:ro $AWS_DOCKER_IMAGE aws s3 sync $LOCAL_PATH $S3_PATH --sse
+  docker run --volumes-from $freeswitch_container_id:ro $AWS_DOCKER_IMAGE aws s3 sync $local_path $s3_path --sse
+fi
