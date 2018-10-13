c1ec814eb312f54ce05e7359ed78d9d27b8a5f35
David Wilkie <dwilkie@gmail.com>
2018-10-13 15:17:00 +0700

    Use latest FreeSWITCH

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,11 +11,6 @@ FREESWITCH_GROUP="daemon"
 set -e
 
 if [ "$1" = 'freeswitch' ]; then
-  if [ -n "$FREESWITCH_CONFIG_S3_PATH" ]; then
-    # Pull FreeSWITCH configuration from S3
-    aws s3 cp --recursive ${FREESWITCH_CONFIG_S3_PATH} ${FREESWITCH_CONTAINER_CONFIG_DIRECTORY}
-  fi
-
   # Setup recordings directory
   mkdir -p ${FREESWITCH_CONTAINER_RECORDINGS_DIRECTORY}
   chown -R "${FREESWITCH_USER}:${FREESWITCH_GROUP}" ${FREESWITCH_CONTAINER_RECORDINGS_DIRECTORY}
