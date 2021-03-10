28a83f78264bf2affcbe66f0cfc1fb7c2c94fc71
David Wilkie <dwilkie@gmail.com>
2021-03-10 11:58:25 +0700

    Push public image (#24)

--- infrastructure/core/ecr.tf
+++ infrastructure/core/ecr.tf
@@ -1,7 +1,10 @@
-resource "aws_ecr_repository" "app" {
-  name                 = "somleng-freeswitch"
+resource "aws_ecrpublic_repository" "app" {
+  repository_name = "somleng-freeswitch"
+  provider = aws.us-east-1
 
-  image_scanning_configuration {
-    scan_on_push = true
+  catalog_data {
+    about_text        = "Somleng FreeSWITCH"
+    architectures     = ["Linux"]
+    description       = "FreeSWITCH configuration optimized for Somleng"
   }
 }
