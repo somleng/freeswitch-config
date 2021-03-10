3d932bced443cc4931c0ad7d023ce0cf17ee136e
David Wilkie <dwilkie@gmail.com>
2021-03-10 11:56:17 +0700

    WIP

--- infrastructure/core/ecr.tf
+++ infrastructure/core/ecr.tf
@@ -1,11 +1,3 @@
-resource "aws_ecr_repository" "app" {
-  name                 = "somleng-freeswitch"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
 resource "aws_ecrpublic_repository" "app" {
   repository_name = "somleng-freeswitch"
   provider = aws.us-east-1
