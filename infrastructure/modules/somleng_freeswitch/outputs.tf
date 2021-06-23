e0bece6e4f251a174f36cb8dd427ec394217f3cf
David Wilkie <dwilkie@gmail.com>
2021-06-23 15:04:41 +0700

    Don't manage carrier IPs here

--- infrastructure/modules/somleng_freeswitch/outputs.tf
+++ infrastructure/modules/somleng_freeswitch/outputs.tf
@@ -2,3 +2,7 @@ output "rayo_password_parameter" {
   value = aws_ssm_parameter.rayo_password
   sensitive = true
 }
+
+output "inbound_sip_trunks_security_group" {
+  value = aws_security_group.appserver
+}
--- infrastructure/production/outputs.tf
+++ infrastructure/production/outputs.tf
@@ -2,3 +2,7 @@ output "rayo_password_parameter" {
   value = module.somleng_freeswitch.rayo_password_parameter
   sensitive = true
 }
+
+output "inbound_sip_trunks_security_group" {
+  value = module.somleng_freeswitch.inbound_sip_trunks_security_group
+}
