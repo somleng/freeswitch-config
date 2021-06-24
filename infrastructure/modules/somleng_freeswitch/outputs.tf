18ae3eec3b13b4c896175afe81a3880f2b080ea5
David Wilkie <dwilkie@gmail.com>
2021-06-24 07:51:25 +0700

    Remove output for security group

--- infrastructure/modules/somleng_freeswitch/outputs.tf
+++ infrastructure/modules/somleng_freeswitch/outputs.tf
@@ -2,7 +2,3 @@ output "rayo_password_parameter" {
   value = aws_ssm_parameter.rayo_password
   sensitive = true
 }
-
-output "inbound_sip_trunks_security_group" {
-  value = aws_security_group.appserver
-}
